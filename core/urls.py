a1e7211e4baa6ee0ef6bd52de994d48e17052910
marioramirez90 <mario-ramirez@hotmail.de>
2026-08-05 12:52:46 +0200

    Fix typo in URL routing for notes API in core urls.py

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('nodes_app.api.urls')),
+    path('api/', include('notes_app.api.urls')),
 ]
